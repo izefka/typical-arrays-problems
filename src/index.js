cd6eb8f0032215470fe149db0502268a8f824885
izefka <54077631+izefka@users.noreply.github.com>
2021-03-14 16:41:25 +0300

    feat: new

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  var min=Number(array[0]);
+  for (var i = 0; i <array.length; i++){
+       if(array[i]<min) {min=array[i];}
+    }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0 ) return 0;
+  var max=Number(array[0]);
+  for (var i = 0; i <array.length; i++){
+       if(array[i]>max) {max=array[i];}
+    }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0;
+  let sum=0;
+  let k=0;
+  for (var i = 0; i <array.length; i++){
+    sum+=array[i];k++}
+   return sum/k;
 }
